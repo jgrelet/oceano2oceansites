551da62ee25d0749e91c317eae85a9ac86ce56b1
jacques Grelet <Jacques.Grelet@ird.fr>
2017-04-13 21:51:37 +0000

    check all config toml files

--- data/alias.cmd
+++ data/alias.cmd
@@ -1,5 +1,11 @@
 # LiteIDE, menu Build/Buil Configurtion, custom/Name: TARGETARGS
 ABRACOS2:
--e --config=config/abraco2.toml --files=data/CTD/abra2/abra2*.cnv
+-e --config=config/abracos2.toml --files=data/CTD/abra2/abra2*.cnv
 PIRATA-FR26:
 -e --config=config/fr26.toml --files=data/CTD/fr26/fr26*.cnv
+PIRATA-FR25:
+-e --config=config/fr25.toml --files=data/CTD/fr25/fr25*.cnv
+PIRATA-FR24:
+-e --config=config/fr24.toml --files=data/CTD/fr24/fr24*.cnv
+CASSIOPEE:
+-e --config=config/oceano2oceansites.toml --files=data/CTD/csp/csp*.cnv
