c92c57aa4e98df4c824d4e094e49faa3b92a038f
jacques Grelet <Jacques.Grelet@ird.fr>
2017-04-13 18:50:47 +0000

    move .toml inside config dir add alias.cmd description file used with LiteIDE build new data skeleton

---
+++ data/alias.cmd
@@ -0,0 +1,5 @@
+# LiteIDE, menu Build/Buil Configurtion, custom/Name: TARGETARGS
+ABRACOS2:
+-e --config=config/abraco2.toml --files=data/abra2/abra2*.cnv
+PIRATA-FR26:
+-e --config=config/fr26.toml --files=data/fr26/fr26*.cnv
