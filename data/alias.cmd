2ed42b9eaccd5768a45903c981f5b1e261e7058b
jacques Grelet <Jacques.Grelet@ird.fr>
2017-04-19 10:18:42 +0000

    add BTL CASSIOPEE arguments line

--- data/alias.cmd
+++ data/alias.cmd
@@ -1,12 +1,15 @@
-# LiteIDE, menu Build/Buil Configurtion, custom/Name: TARGETARGS
-ABRACOS2:
+# LiteIDE, menu Build/Build configurtion, custom/Name: TARGETARGS
+CTD ABRACOS2:
 -e --config=config/abracos2.toml --files=data/CTD/abra2/abra2*.cnv
-PIRATA-FR26:
+CTD PIRATA-FR26:
 -e --config=config/fr26.toml --files=data/CTD/fr26/fr26*.cnv
-PIRATA-FR25:
+CTD PIRATA-FR25:
 -e --config=config/fr25.toml --files=data/CTD/fr25/fr25*.cnv
-PIRATA-FR24:
+CTD PIRATA-FR24:
 -e --config=config/fr24.toml --files=data/CTD/fr24/fr24*.cnv
-CASSIOPEE:
+CTD CASSIOPEE:
 -e --config=config/cassiopee.toml --files=data/CTD/csp/csp*.cnv
 -e --files=data/CTD/csp/csp*.cnv
+BTL CASSIOPEE:
+-e --config=config/cassiopee.toml --files=data/BTL/csp/csp*.btl
+-e --files=data/BTL/csp/csp*.btl
