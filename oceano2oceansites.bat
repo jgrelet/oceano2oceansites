1d1d5bc9c8e79342daf5462f5ee17e1b94720cdb
jacques Grelet <Jacques.Grelet@ird.fr>
2017-06-21 17:08:45 -0300

    add batch file for test and run

---
+++ oceano2oceansites.bat
@@ -0,0 +1,10 @@
+pwd
+SET "DRIVE=M:"
+set "CRUISE=SARGASSE"
+set "CONFIG=sargasse.toml"
+set "PREFIX=S"
+cd %DRIVE%\%CRUISE%\data-processing\CTD
+pwd
+oceano2oceansites -c %DRIVE%\%CRUISE%\data-processing\%CONFIG% -r %DRIVE%\%CRUISE%\local\code_roscop.csv -d --files=%DRIVE%\%CRUISE%\data-processing\CTD\data\cnv\%PREFIX%*.cnv 
+rem %DRIVE%\%CRUISE%\local\sbin\oceano2oceansites -c %DRIVE%\%CRUISE%\data-processing\%CONFIG% -r %DRIVE%\%CRUISE%\local\code_roscop.csv -e -a --files=%DRIVE%\%CRUISE%\data-processing\CTD\data\cnv\%PREFIX%*.cnv 
+pause
