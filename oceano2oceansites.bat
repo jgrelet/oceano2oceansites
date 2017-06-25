49825ee7c033dd89bad7bd74a67bbf4e04adcbbe
jacques Grelet <Jacques.Grelet@ird.fr>
2017-06-25 18:49:03 -0300

    use echo option

--- oceano2oceansites.bat
+++ oceano2oceansites.bat
@@ -5,6 +5,6 @@ set "CONFIG=sargasse.toml"
 set "PREFIX=S"
 cd %DRIVE%\%CRUISE%\data-processing\CTD
 pwd
-oceano2oceansites -c %DRIVE%\%CRUISE%\data-processing\%CONFIG% -r %DRIVE%\%CRUISE%\local\code_roscop.csv -d --files=%DRIVE%\%CRUISE%\data-processing\CTD\data\cnv\%PREFIX%*.cnv 
+oceano2oceansites -c %DRIVE%\%CRUISE%\data-processing\%CONFIG% -r %DRIVE%\%CRUISE%\local\code_roscop.csv -e --files=%DRIVE%\%CRUISE%\data-processing\CTD\data\cnv\%PREFIX%*.cnv 
 rem %DRIVE%\%CRUISE%\local\sbin\oceano2oceansites -c %DRIVE%\%CRUISE%\data-processing\%CONFIG% -r %DRIVE%\%CRUISE%\local\code_roscop.csv -e -a --files=%DRIVE%\%CRUISE%\data-processing\CTD\data\cnv\%PREFIX%*.cnv 
 pause
